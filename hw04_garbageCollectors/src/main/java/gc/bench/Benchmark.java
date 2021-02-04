a6f0f974c9d64f1b48117243aeaee3d1aac9e442
DmitryLapitsky <56531706+DmitryLapitsky@users.noreply.github.com>
2021-02-04 21:04:37 +0300

    домашнее задание с garbage collections

---
+++ hw04_garbageCollectors/src/main/java/gc/bench/Benchmark.java
@@ -0,0 +1,45 @@
+package gc.bench;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Benchmark implements gc.bench.BenchmarkMBean {
+    private final int loopCounter;
+    private volatile int size = 0;
+
+    public Benchmark(int loopCounter) {
+        this.loopCounter = loopCounter;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public void setSize(int size) {
+        System.out.println("new size:" + size);
+        this.size = size;
+    }
+
+    private int id = 0;
+
+    public int getId() {
+        return id;
+    }
+
+    void getOOM() throws InterruptedException {
+        for (int idx = 0; idx < loopCounter; idx++) {
+            id = idx;
+            List<String> integers = new ArrayList<>();
+            for (int i = 0; i < size * idx; i++) {//17000
+                integers.add(new String(new char[0]));
+                if (i % 2 == 0) {
+                    integers.set(i, null);
+                }
+            }
+            Thread.sleep(10);
+        }
+    }
+}
